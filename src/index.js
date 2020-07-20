6551619d7a581b892ca15ca95ca836bd1d2b4f92
Victor Batigin <bixs85@gmail.com>
2020-07-20 22:00:11 +0300

    try loader

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store from './redux/store';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import './index.css';
 
 import App from './App';
